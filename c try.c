2a74d60fb80479b0864de2575cd6d85aaa1a767e
QUANChan07 <Z1574340246@outlook.com>
2023-10-01 13:28:04 +0800

    think

---
+++ c try.c
@@ -0,0 +1,13 @@
+#include<stdio.h>
+int main(a,b,c)
+{
+    int z;
+    scanf("%d,%d,%d",&a,&b,&c);
+    if (a<b)a=b;
+    else;
+    if (c<a)c=a;
+    else;
+    z=c;
+    printf("%d\n",z);
+    return 0;
+}
