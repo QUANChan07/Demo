0ec1b9ed2716810276c4ef524c5205585a82b82a
QUANChan07 <Z1574340246@outlook.com>
2023-10-01 17:36:26 +0800

    try

---
+++ c A.c
@@ -0,0 +1,10 @@
+#include<stdio.h>
+#include<stdlib.h>
+main()
+{
+    char c1,c2,c3,c4,c5;
+    char C,h,i,n,a;
+    c1=C,c2=h,c3=i,c4=n,c5=a;
+    printf("%c,%c,%c,%c,%c\n",char(int(c1)+4),char(int(c2)+4),char(int(c3)+4),char(int(c4)+4),char(int(c5)+4));
+    return 0;
+}
